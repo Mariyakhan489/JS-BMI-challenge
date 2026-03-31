3e82f89590cd2ea389d63a512a431356fddd436e
Mariyakhan489 <maryakhan489@gmail.com>
2026-03-31 02:06:13 -0700

    bmi challange

---
+++ script.js
@@ -0,0 +1,37 @@
+//  Coding Challenge # 1
+
+//  (Task-1) Mark's BMI
+let markWeight = 78;
+let markHeight = 1.69;
+console.log(`Mark weight is ${markWeight} kg`);
+console.log(`Mark height is ${markHeight} m`);
+
+let markBmi = markWeight / (markHeight * markHeight);
+console.log(`Mark BMI is ${markBmi}`);
+
+// John's BMI
+let johnWaight = 92;
+let johnHeight = 1.95;
+console.log(`John waight is ${johnWaight} kg`);
+console.log(`John height is ${johnHeight} m`);
+
+let johnBmi = johnWaight / (johnHeight * johnHeight);
+console.log(`John BMI is ${johnBmi}`);
+
+//  (Task-2) Mark's BMI
+let markMass = 95;
+let markTallness = 1.88;
+console.log(`Mark Weight is ${markMass} kg`);
+console.log(`Mark Height is ${markTallness} m`);
+ 
+let mBmi = markMass / (markTallness * markTallness);
+console.log(`BMI of Mark is ${mBmi}`);
+
+//John's BMI
+let johnMass = 95;
+let johnTallness = 1.88;
+console.log(`John Weight is ${johnMass} kg`);
+console.log(`John Height is ${johnTallness} m`);
+ 
+let jBmi = johnMass / (johnTallness * johnTallness);
+console.log(`BMI of John is ${jBmi}`);
